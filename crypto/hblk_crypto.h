38e80d3150f98bb3690164738db9c0ad2222fdf8
kamalaaouad <kamalaaouad1999@gmail.com>
2021-03-30 01:29:09 +0000

    ec_create

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,7 +1,12 @@
 #ifndef HBLK_CRYPTO_H
 #define HBLK_CRYPTO_H
 #define SHA256_DIGEST_LENGTH 64
+#include <openssl/ec.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
 #include <openssl/sha.h>
 #include <stdint.h>
+#define EC_CURVE        NID_secp256k1
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY * ec_create(void);
 #endif
