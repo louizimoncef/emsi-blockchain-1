bf5db2cd71be5e486132504ea1fa6c9bc8d16fc1
kamalaaouad <kamalaaouad1999@gmail.com>
2021-04-02 15:04:34 +0000

    save peer key in two file key.pem and key_pub.pem in folder aaouad

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -5,11 +5,16 @@
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/sha.h>
+#include <sys/stat.h>
 #include <stdint.h>
+#include <string.h>
 #define EC_CURVE        NID_secp256k1
 #define EC_PUB_LEN	65
+#define PUB_FILENAME	"key_pub.pem"
+#define PRI_FILENAME	"key.pem"
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY * ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
 #endif
