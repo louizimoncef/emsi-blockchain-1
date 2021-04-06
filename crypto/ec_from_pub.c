61e265a3a34ecddcf9f8c108e474daa52d78263c
kamalaaouad <kamalaaouad1999@gmail.com>
2021-04-06 21:48:33 +0000

    signature, verify signature and makefile

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,36 +1,33 @@
 #include "hblk_crypto.h"
 
-
+/**
+ * ec_from_pub - generates EC_KEY from pub key
+ * @pub: the pub key in buffer
+ * Return: pointer to generated EC_KEY struct or NULL
+ */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key;
-	EC_GROUP *group;
-	EC_POINT *p;
+	EC_POINT *point;
 
-	if (!pub)
-		return NULL;
+	if (!pub) /* Needed? */
+		return (NULL);
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
-		return NULL;
-	group = (EC_GROUP *)EC_KEY_get0_group(key);
-	p = EC_POINT_new(group);
-	if (!p)
+		return (NULL);
+	point = EC_POINT_new(EC_KEY_get0_group(key));
+	if (!point)
 	{
 		EC_KEY_free(key);
-		return NULL;
+		return (NULL);
 	}
-	if (!EC_POINT_oct2point(group, p, pub, EC_PUB_LEN, NULL))
-         {	
-	      EC_KEY_free(key);
-              EC_POINT_free(p);
-              return NULL;
-          }
-	if (!EC_KEY_set_public_key(key, p))
-	  {      	
-              EC_KEY_free(key);
-              EC_POINT_free(p);
-              return NULL;
-           }
-	EC_POINT_free(p);
-	return key;
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL)
+		|| !EC_KEY_set_public_key(key, point))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
+		return (NULL);
+	}
+	EC_POINT_free(point);
+	return (key);
 }
